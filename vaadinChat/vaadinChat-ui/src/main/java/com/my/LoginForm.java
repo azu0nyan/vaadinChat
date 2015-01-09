2fd623c3ec6ac18506afc1f3019e0b41b56bb378
azu0nyan <nikgavgav@gmail.com>
2015-01-09 19:19:15 +0300

    login form try

--- vaadinChat/vaadinChat-ui/src/main/java/com/my/LoginForm.java
+++ vaadinChat/vaadinChat-ui/src/main/java/com/my/LoginForm.java
@@ -1,7 +1,106 @@
 package com.my;
 
+
+import com.vaadin.data.validator.EmailValidator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.Reindeer;
+
 /**
  * Created by azu on 09.01.2015.
  */
-public class LoginForm {
+public class LoginForm extends CustomComponent implements View, Button.ClickListener {
+
+    public static final String NAME = "login";
+
+    private final TextField user;
+
+    private final PasswordField password;
+
+    private final Button loginButton;
+
+    public LoginForm() {
+        // Create the user input field
+        user = new TextField("User:");
+        user.setWidth("300px");
+        user.setRequired(true);
+        user.setInputPrompt("Your username (eg. joe@email.com)");
+        user.addValidator(new EmailValidator(
+                "Username must be an email address"));
+        user.setInvalidAllowed(false);
+
+        // Create the password input field
+        password = new PasswordField("Password:");
+        password.setWidth("300px");
+        password.addValidator(new PasswordValidator());
+        password.setRequired(true);
+        password.setValue("");
+        password.setNullRepresentation("");
+
+        // Create login button
+        loginButton = new Button("Login", this);
+
+        // Add both to a panel
+        VerticalLayout fields = new VerticalLayout(user, password, loginButton);
+        fields.setCaption("Please login to access the application. (test@test.com/passw0rd)");
+        fields.setSpacing(true);
+        fields.setMargin(new MarginInfo(true, true, true, false));
+        fields.setSizeUndefined();
+
+        // The view root layout
+        VerticalLayout viewLayout = new VerticalLayout(fields);
+        viewLayout.setSizeFull();
+        viewLayout.setComponentAlignment(fields, Alignment.MIDDLE_CENTER);
+        viewLayout.setStyleName(Reindeer.LAYOUT_BLUE);
+        setCompositionRoot(viewLayout);
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+        user.focus();
+    }
+
+    @Override
+    public void buttonClick(Button.ClickEvent event) {
+
+        //
+        // Validate the fields using the navigator. By using validors for the
+        // fields we reduce the amount of queries we have to use to the database
+        // for wrongly entered passwords
+        //
+        if (!user.isValid() || !password.isValid()) {
+            return;
+        }
+
+        String username = user.getValue();
+        String password = this.password.getValue();
+
+        //
+        // Validate username and password with database here. For examples sake
+        // I use a dummy username and password.
+        //
+        boolean isValid = username.equals("test@test.com")
+                && password.equals("passw0rd");
+
+        if (isValid) {
+
+            // Store the current user in the service session
+            getSession().setAttribute("user", username);
+
+            // Navigate to main view
+            getUI().getNavigator().navigateTo(SimpleLoginMainView.NAME);//
+
+        } else {
+
+            // Wrong password clear the password field and refocuses it
+            this.password.setValue(null);
+            this.password.focus();
+
+        }
+    }
 }
+
+
+
